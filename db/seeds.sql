a7d2a8dacd19c6a1aa11ddb3f446b54e631cc8a6
Katie DeAngelis <katie@Katies-MacBook-Pro.local>
2018-01-08 19:16:00 -0500

    Seeded database.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burgerName) VALUES ('Gourdon-Hamsey Burger');
+INSERT INTO cats (burgerName) VALUES ('The Silentil Night Burger');
+INSERT INTO cats (burgerName, devoured) VALUES ('Poutine on the Ritz Burger', true);
+INSERT INTO cats (burgerName, devoured) VALUES ('Poblano Picasso Burger', true);
+INSERT INTO cats (burgerName, devoured) VALUES ('Beets of Burden Burger', true);
+INSERT INTO cats (burgerName) VALUES ('The Human Polenta-pede Burger');
