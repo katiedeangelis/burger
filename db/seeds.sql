22b390795fb2c37e539d2ebf593daff5456553e0
Katie DeAngelis <katie@Katies-MacBook-Pro.local>
2018-01-08 19:24:02 -0500

    setup the code to connect Node to MySQL.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (burgerName) VALUES ('Gourdon-Hamsey Burger');
-INSERT INTO cats (burgerName) VALUES ('The Silentil Night Burger');
-INSERT INTO cats (burgerName, devoured) VALUES ('Poutine on the Ritz Burger', true);
-INSERT INTO cats (burgerName, devoured) VALUES ('Poblano Picasso Burger', true);
-INSERT INTO cats (burgerName, devoured) VALUES ('Beets of Burden Burger', true);
-INSERT INTO cats (burgerName) VALUES ('The Human Polenta-pede Burger');
+INSERT INTO burgers (burgerName) VALUES ('The Silentil Night Burger');
+INSERT INTO burgers (burgerName, devoured) VALUES ('Poutine on the Ritz Burger', true);
+INSERT INTO burgers (burgerName, devoured) VALUES ('Poblano Picasso Burger', true);
+INSERT INTO burgers (burgerName, devoured) VALUES ('Beets of Burden Burger', true);
+INSERT INTO burgers (burgerName) VALUES ('The Human Polenta-pede Burger');
